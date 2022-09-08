97d84e49eb8423f00a162b13e397c060c96f7fdb
Hamza <hamza@snappymob.com>
2022-09-08 15:15:53 +0800

    Add arangodb

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express, { Application, Request, Response, NextFunction } from "express"
-import bodyParser from "body-parser"
-import { getAll, create } from './controllers/charge-master';
+import express, { Application, Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser'
+import { getAll, create } from './controllers/charge-master'
 
 const app: Application = express()
 const port = 8080
@@ -11,9 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (request: Request, response: Response) => {
     response.send('RCM server pinged')
 })
-
-app.post('/user', create)
-app.get('/users', getAll)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
