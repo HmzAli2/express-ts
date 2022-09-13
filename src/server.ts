6c12d94e158abebd5c0e8391d1c876ba98d8db32
Hamza <hamza@snappymob.com>
2022-09-13 14:15:17 +0800

    db

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-// import express, { Application, Request, Response, NextFunction } from 'express'
-// import bodyParser from 'body-parser'
-// import { appConfig } from './config'
-// import { connect } from './adapters/db'
+import express, { Application, Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser'
+import { appConfig } from './config'
+import { connect } from './adapters/db'
 
-// connect()
+connect()
 
-// const app: Application = express()
+const app: Application = express()
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.get('/', (request: Request, response: Response) => {
-//     response.send('RCM server pinged')
-// })
+app.get('/', (request: Request, response: Response) => {
+    response.send('RCM server pinged')
+})
 
-// app.listen(appConfig.port, () => {
-//     console.log(`App running on port ${appConfig.port}`)
-// })
+app.listen(appConfig.port, () => {
+    console.log(`App running on port ${appConfig.port}`)
+})
