b0fbeaf86e8d9604ce24ef3a3556b2a5024bc1ad
Vibek Dutta <vikiisbck@gmail.com>
2022-09-13 11:47:15 +0530

    logger file updated

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,54 +1,47 @@
-const info = (namespace: string, message: string, object?: any) => {
+export const Info = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.info(
+    console.log(
       `[${getTimeStamp()}] [INFO] [${namespace}] ${message}`,
       object
     );
   } else {
-    console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+    console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
   }
 };
 
-const warn = (namespace: string, message: string, object?: any) => {
+export const Warn = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.warn(
+    console.log(
       `[${getTimeStamp()}] [WARN] [${namespace}] ${message}`,
       object
     );
   } else {
-    console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
+    console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
   }
 };
 
-const error = (namespace: string, message: string, object?: any) => {
+export const Error = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.error(
+    console.log(
       `[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`,
       object
     );
   } else {
-    console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+    console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
   }
 };
 
-const debug = (namespace: string, message: string, object?: any) => {
+export const Debug = (namespace: string, message: string, object?: any) => {
   if (object) {
-    console.debug(
+    console.log(
       `[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`,
       object
     );
   } else {
-    console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+    console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
   }
 };
 
 const getTimeStamp = (): string => {
   return new Date().toISOString();
-};
-
-export default {
-  info,
-  warn,
-  error,
-  debug,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
 import { appConfig } from './config/config'
+import { Info, Debug, Warn, Error } from './config/logger';
 import express, { Application, Request, Response, NextFunction } from 'express'
 
 const app: Application = express()
@@ -8,9 +9,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (request: Request, response: Response) => {
+    Info("RCM server pinged", "Status 200")
     response.send('RCM server pinged')
 })
 
 app.listen(appConfig.port, () => {
-    console.log(`App running on port ${appConfig.port}`)
+    console.info(`App running on port ${appConfig.port}`)
 })
